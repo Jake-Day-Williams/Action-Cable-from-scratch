28af0c25d8f61cd3d14751fa5588314d59261f40
Jake Day Williams <jake@wildacademy.com>
2016-04-12 03:19:18 +0000

    Added devise, devise User, migration, AddUserToMessages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+  config.action_cable.url = "wss://devpoint-week-9-jakeday.c9users.io:8080/cable"
+  config.action_cable.allowed_request_origins = ['http://devpoint-week-9-jakeday.c9users.io:8080']
   # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.url = 'wss://devpoint-week-9-jakeday.c9users.io'
+   config.action_cable.allowed_request_origins = [ 'https://devpoint-week-9-jakeday.c9users.io', /http:\/\/devpoint-week-9-jakeday.c9users.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
